2682e5db1c4ded06604019307f299e944d5e8852
awesomehaze <kaiketchum@gmail.com>
2020-04-11 14:37:18 -0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-print(cv2.__version__)
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -8,11 +7,28 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(gray,200,255,0)
+
+    M = cv2.moments(thresh)
+
+    try:
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+
+        cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+        cv2.putText(frame, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    except:
+        pass
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
+    cv2.imshow('picture',thresh)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    
 
 # When everything done, release the capture
 cap.release()
