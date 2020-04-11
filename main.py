fa449797ead2541596a6b5c17dfe9177f639d6e2
LCHS iTeam <team@lciteam.club>
2020-04-11 14:44:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import cv2
-cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+def findHandPos (cameraResolution):
+    cap = cv2.VideoCapture(1)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(gray,200,255,0)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame=cv2.flip(frame,1)
 
-    M = cv2.moments(thresh)
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret,thresh = cv2.threshold(gray,200,255,0)
 
-    try:
-        # calculate x,y coordinate of center
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+        roiLeft=thresh[0:cameraResolution[0]//3, 0:cameraResolution[1]]
+        roiRight=thresh[0:cameraResolution[0]//3, cameraResolution[0]:cameraResolution[1]]
+
+        MLeft = cv2.moments(roiLeft)
+        MRight = cv2.moments(roiRight)
+
+        
+            # calculate x,y coordinate of center
+        if (MLeft["m00"] != 0):
+            cX = int(MLeft["m10"] / MLeft["m00"])
+            cY = int(MLeft["m01"] / MLeft["m00"])
 
 
-        cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
-        cv2.putText(frame, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    except:
-        pass
+            cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+            cv2.putText(frame, "centroid Left", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    cv2.imshow('picture',thresh)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        else:
+            print(MLeft["m00"])
+        
 
-    
+        try:
+            # calculate x,y coordinate of center
+            cX = int(MRight["m10"] / MRight["m00"])
+            cY = int(MRight["m01"] / MRight["m00"])
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+            cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+            cv2.putText(frame, "centroid Right", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        except:
+            pass
+
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        cv2.imshow('picture',roiLeft)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+findHandPos([1280,720])
