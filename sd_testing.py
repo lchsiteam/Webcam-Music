735cd5468f9166e1167ce9206452f879d6478d38
not a cat <pandinobud@gmail.com>
2020-04-12 01:57:58 -0700

    comments

--- sd_testing.py
+++ sd_testing.py
@@ -6,20 +6,19 @@ import data
 import time
 import builtins
 
+#refresh rate
 fs = 15000
 sd.default.samplerate = fs
 sd.default.channels = 2
 
-seconds = 10
-
+#a linear scale thing for making the sound
 t = np.linspace(0, 1, fs * 1, False, dtype=np.float32) 
 
+#the stream
 s = sd.Stream(latency='low', blocksize=1, samplerate=fs, channels=1) 
 s.start() 
 
-start_freq = None
-end_freq = None
-
+#the sign function - returns -1 for all negative input, 1 for positive input, and 0 for 0
 def sgn(num): 
     maximum = 0.1
 
@@ -30,11 +29,11 @@ def sgn(num):
     else: 
         return 0
 
+#the loop that handles the audio
 def run(): 
     index = 0
     #prev_n = None
     #current_freq = 440
-    current_freq = None
 
     while True: 
         rounds, a_index = divmod(index, len(t)) 
@@ -50,14 +49,14 @@ def run():
 
         pre_amp = math.sin(thing) 
 
-        if data.waveform == 'Square': 
+        if data.waveform == 'Square': #square wave
             pre_amp = sgn(pre_amp) 
-        elif data.waveform == 'Triangle': 
+        elif data.waveform == 'Triangle': #triangle wave
             ref = a * current_freq - math.floor(a * current_freq) 
             
             pre_amp = abs(ref - round(ref)) * 2
 
-        actual_n = n = data.volume * pre_amp / 30
+        actual_n = n = data.volume * pre_amp / 30 #here's where amplitude comes in
 
         '''
         if (prev_n is not None and (prev_n <= 0 and n >= 0 or prev_n >= 0 and n <= 0)): 
@@ -70,9 +69,9 @@ def run():
         #print(n) 
         #a, overflowed = s.read(1) 
 
-        array = np.array((actual_n,), np.float32) 
+        array = np.array((actual_n,), np.float32) #the thing that gets written to the stream to produce sound
 
-        s.write(array) 
+        s.write(array) #writes to stream to produce sound
 
         index += 1
 
